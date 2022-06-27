3247fbb6ef9b9ecfce1b290a782efb537a545929
bevsta <108017385+bevsta@users.noreply.github.com>
2022-06-27 09:38:38 +0300

    -Smth deteted

--- cars/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-from cars.views import *
-
-urlpatterns = [
-    path('imports/', ItemCreateView.as_view()),
-    path('all/', ItemsListView.as_view()),
-    path('nodes/<str:pk>', ItemDetailView.as_view())
-]
